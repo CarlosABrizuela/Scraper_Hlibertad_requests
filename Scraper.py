aee2a7ae24757eb328bac5d3acfd78cc48d7914e
CarlosABrizuela <carlosalfredobrizuela@hotmail.com>
2023-10-26 14:35:09 -0300

    with get_categories the class can get all categories formatted in order to process each one at a time

--- Scraper.py
+++ Scraper.py
@@ -1,8 +1,8 @@
-import json
+from sre_parse import CATEGORIES
 import requests
 import time
 from requests.exceptions import ProxyError
-# import concurrent.futures
+import concurrent.futures
 
 class ScraperHLibertad:
     def __init__(self, config):
@@ -49,19 +49,77 @@ class ScraperHLibertad:
         return None
     
     def run(self):
-        """ Ejecución del codigo
+        """ main method. 
         """
-            
-        # with concurrent.futures.ThreadPoolExecutor(max_workers=self.config['numero_hilos']) as executor:
-        #     executor.map(, lista_registros)
+        all_categories = self.get_categories()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.config['thread_number']) as executor:
+            executor.map(self.process_category, all_categories)
 
         self.close()
     
-    def obtener_categorias(self):
-        """Obtener la lista de categorias y darles el formato para el procesamiento
+    def process_category(self, category):
+        """Obtener los productos para cada categoria
         """
-        pass
+        print(category['nombre'])
 
+    def get_categories(self):
+        """Obtener la lista de categorias y darles el formato para el procesamiento
+        Necesitamos el codigo para usar en la consulta.
+        formato:
+        {
+            "nombre": "Tecnologia",
+            "sub_categorias": [
+                    {
+                        "nombre": "Tecnologia/TV Y VIDEO/TV LED Y SMART TV",
+                        "codigo": "1/33/34"
+                    }, ...
+                ], ...
+        }
+        """
+        categories_json = self.fetch(self.config['categories_url'])
+        if categories_json:
+            return self.process_list_categories(categories_json)
+        # else:
+        #     return self.process_list_categories(get_local_categories())
+
+    def process_list_categories(self, categories_json):
+        # acumula en una lista las categorias de ultimo nivel, para cada categoria de primer nivel.
+        lista_sup= []
+        for category in categories_json:
+            sub_categories = []
+            if category["hasChildren"]:
+                nombre = category["name"]
+                codigo = str(category["id"])
+                self.process_list_subcategories(category["children"], sub_categories, nombre, codigo)
+
+            categoria_dict_sup ={
+                    "nombre": category['name'],
+                    "sub_categorias": sub_categories
+                }
+            lista_sup.append(categoria_dict_sup)
+            
+        return lista_sup
+
+    def process_list_subcategories(self, categorias_json, list_subcats, nombre, codigo):
+        # Devuelve las categorias hojas, o esas que no tienen categorias hijas.
+        nombre_prev =nombre
+        codigo_prev = codigo
+        for index, categoria in enumerate(categorias_json):
+            nombre = nombre +"/"+ categoria["name"]
+            codigo = codigo +"/"+ str(categoria["id"])
+            if categoria["hasChildren"]:
+                self.process_list_subcategories(categoria["children"], list_subcats, nombre, codigo)
+            else:
+                categoria_dict = {
+                    "nombre": nombre,
+                    "codigo": codigo
+                }
+                list_subcats.append(categoria_dict)
+            nombre = nombre_prev
+            codigo = codigo_prev
+        
+            if index == len(categorias_json) - 1:
+                return
 
     def crear_csv(self):
         """
